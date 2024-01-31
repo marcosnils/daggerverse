afc2add7032bd2c38c2c9c93ac9963ef95c45dbf
Marcos Lilljedahl <marcosnils@gmail.com>
2024-01-31 13:18:21 -0300

    fix hello
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- cami/main.go
+++ cami/main.go
@@ -4,5 +4,5 @@ type Cami struct{}
 
 // example usage: "dagger call hello"
 func (m *Cami) Hello() *Container {
-	return dag.Container().From("alpine:latest").WithExec([]string{"hello", "cami"})
+	return dag.Container().From("alpine:latest").WithExec([]string{"echo", "hello cami"})
 }
