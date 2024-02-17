9c609446134e4bb1f1fa2d67e01b7ffd2f1b2d3a
Marcos Lilljedahl <marcosnils@gmail.com>
2024-02-17 13:38:23 -0300

    add app channel address as argument
    also bumped dagger to the latest version
    
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- dapr/main.go
+++ dapr/main.go
@@ -5,21 +5,18 @@ import (
 	"strconv"
 )
 
-type Component struct {
-	Name string
-	Type string
-}
-
-type Dapr struct {
-	components []*Component
-}
+type Dapr struct{}
 
 func (m *Dapr) Dapr(
 	ctx context.Context,
 	appId string,
-	appPort Optional[int],
-	componentsPath Optional[*Directory],
-) *Service {
+	// +optional
+	appPort *int,
+	// +optional
+	appChannelAddress *string,
+	// +optional
+	componentsPath *Directory,
+) *Container {
 	// daprPlacement := dag.Container().From("daprio/placement:1.12.2").
 	// WithEntrypoint([]string{"./placement"}).
 	// WithExposedPort(50005).
@@ -27,14 +24,18 @@ func (m *Dapr) Dapr(
 
 	args := []string{"./daprd", "-components-path", "/components", "-app-id", appId, "-log-level", "debug"}
 
-	if p, ok := appPort.Get(); ok {
-		args = append(args, "-app-port", strconv.Itoa(p))
+	if appPort != nil {
+		args = append(args, "-app-port", strconv.Itoa(*appPort))
+	}
+
+	if appChannelAddress != nil {
+		args = append(args, "-app-channel-address", *appChannelAddress)
 	}
 
 	dapr := dag.Container().From("daprio/daprd:1.12.2").
 		With(func(c *Container) *Container {
-			if d, ok := componentsPath.Get(); ok {
-				c = c.WithDirectory("/components", d)
+			if componentsPath != nil {
+				c = c.WithDirectory("/components", componentsPath)
 			}
 			return c
 		}).
@@ -42,30 +43,6 @@ func (m *Dapr) Dapr(
 		// TODO set app id to right value
 		WithEntrypoint(args).
 		WithExposedPort(50001).
-		WithExposedPort(3500).AsService()
+		WithExposedPort(3500)
 	return dapr
 }
-
-func (m *Dapr) WithComponents(c ...*Component) *Dapr {
-	m.components = append(m.components, c...)
-	return m
-}
-
-func (m *Dapr) Run(dir *Directory, componentsPath Optional[*Directory]) *Container {
-	return dag.Go().WithSource(dir).Container().
-		WithServiceBinding("dapr", m.Dapr(context.Background(), "app", OptEmpty[int](), componentsPath)).
-		WithEnvVariable("DAPR_GRPC_PORT", "50001").
-		WithEnvVariable("DAPR_GRPC_ENDPOINT", "dapr").
-		WithExec([]string{"go", "run", "app.go"})
-}
-
-//func (m *Dapr) GoWithDapr(dir *Directory) *Container {
-//return dag.Go().Container().
-//With(m.WithComponents(
-//&Component{"kvstore", "state.in-memory"},
-//&Component{"orderpubsub", "pubsub.in-memory"},
-//).Start).
-//WithDirectory("/app", dir).
-//WithWorkdir("/app").
-//WithExec([]string{"go", "run", "app.go"})
-//}
