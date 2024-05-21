6c0efca955039160d127e466889ccde0b2797312
Marcos Lilljedahl <marcosnils@gmail.com>
2024-05-21 13:47:34 -0300

    make better example
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- gptools/examples/main.go
+++ gptools/examples/main.go
@@ -1,9 +1,18 @@
 package main
 
-import "context"
+import (
+	"context"
+)
 
 type Examples struct{}
 
-func (m *Examples) GPToolsYtChat(ctx context.Context, openaiApiKey *Secret, url, question string) (string, error) {
-	return dag.Gptools().YtChat(ctx, openaiApiKey, url, question)
+// example on how to return a transcript from a video file
+func (m *Examples) GptoolsTranscript(
+	ctx context.Context,
+	// free to use movie https://mango.blender.org/about/
+	//+default="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"
+	url string,
+) (string, error) {
+	video := dag.HTTP(url)
+	return dag.Gptools().Transcript(video).Contents(ctx)
 }
