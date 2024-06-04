4439fa3504edd89b3400666959986d927e645f85
Marcos Lilljedahl <marcosnils@gmail.com>
2024-06-04 12:06:31 -0300

    add more python examples
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- gptools/examples/python/src/main/__init__.py
+++ gptools/examples/python/src/main/__init__.py
@@ -1,9 +1,42 @@
 import dagger
 from dagger import dag, function, object_type
+from collections.abc import Coroutine
 
 
 @object_type
 class Python:
+    @function
+    async def gptools(
+        self,
+        openai_api_key: dagger.Secret,
+        question: str,
+    ) -> str:
+        """example on how to run a full e2e RAG model across different types of
+        documents in a directory"""
+        nixPaper = dag.http(
+            "https://edolstra.github.io/pubs/nspfssd-lisa2004-final.pdf",
+        )
+        foxImage = dag.http(
+            "https://fsquaredmarketing.com/wp-content/uploads/2024/04/bitter-font.png",
+        )
+        return await dag.gptools().rag(
+            openai_api_key,
+            dag.directory()
+            .with_file("nix-paper.pdf", nixPaper)
+            .with_file("image.png", foxImage),
+            question,
+        )
+
+    @function
+    async def gptools_transcript(
+        self,
+        url: str = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4",
+    ) -> str:
+        """example on how to get a transcript for a video and"""
+        # TODO: call transcript once python gen is fixed
+        # video = dag.http(url)
+        return await dag.gptools().audio(url).contents()
+
     @function
     async def gptools_transcript__directory(
         self,
