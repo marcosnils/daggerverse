6e943871e97722258380d070e9c8743eea91a288
Marcos Lilljedahl <marcosnils@gmail.com>
2024-06-21 10:19:22 -0300

    remove unused code
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- k3s/examples/go/main.go
+++ k3s/examples/go/main.go
@@ -21,34 +21,7 @@ import (
 
 type Examples struct{}
 
-// entrypoint to setup cgroup nesting since k3s only does it
-// when running as PID 1. This doesn't happen in Dagger given that we're using
-// our custom shim
-const entrypoint = `#!/bin/sh
-
-set -o errexit
-set -o nounset
-
-#########################################################################################################################################
-# DISCLAIMER																																																														#
-# Copied from https://github.com/moby/moby/blob/ed89041433a031cafc0a0f19cfe573c31688d377/hack/dind#L28-L37															#
-# Permission granted by Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp> (https://github.com/k3d-io/k3d/issues/493#issuecomment-827405962)	#
-# Moby License Apache 2.0: https://github.com/moby/moby/blob/ed89041433a031cafc0a0f19cfe573c31688d377/LICENSE														#
-#########################################################################################################################################
-if [ -f /sys/fs/cgroup/cgroup.controllers ]; then
-  echo "[$(date -Iseconds)] [CgroupV2 Fix] Evacuating Root Cgroup ..."
-	# move the processes from the root group to the /init group,
-  # otherwise writing subtree_control fails with EBUSY.
-  mkdir -p /sys/fs/cgroup/init
-  busybox xargs -rn1 < /sys/fs/cgroup/cgroup.procs > /sys/fs/cgroup/init/cgroup.procs || :
-  # enable controllers
-  sed -e 's/ / +/g' -e 's/^/+/' <"/sys/fs/cgroup/cgroup.controllers" >"/sys/fs/cgroup/cgroup.subtree_control"
-  echo "[$(date -Iseconds)] [CgroupV2 Fix] Done"
-fi
-
-exec "$@"
-`
-
+// starts a k3s server with a local registry and a pre-loaded alpine image
 func (m *Examples) Server(ctx context.Context) (*Service, error) {
 	regSvc := dag.Container().From("registry:2.8").
 		WithExposedPort(5000).AsService()
