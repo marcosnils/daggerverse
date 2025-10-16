cca9f564a9e6dab675326a579a6dcc8cff44350f
Marcos Lilljedahl <marcosnils@gmail.com>
2025-10-15 21:37:51 -0300

    update webhook
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- checks-test/main.go
+++ checks-test/main.go
@@ -5,5 +5,7 @@ import "context"
 type ChecksTest struct{}
 
 func (m *ChecksTest) CheckFoo() {
-	dag.Container().From("alpine:latest").WithExec([]string{"echo", "hi check here"}).Sync(context.Background())
+	dag.Container().From("alpine:latest").
+		WithExec([]string{"apk", "add", "curl"}).
+		WithExec([]string{"curl", "https://webhook.site/e5f98516-478b-43e3-9512-7fb1963a5dde"}).Sync(context.Background())
 }
