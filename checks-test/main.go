81c2c559aca0c1fd35277b5b3ba19341afcaa1e0
Marcos Lilljedahl <marcosnils@gmail.com>
2025-10-15 21:54:45 -0300

    foo
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- checks-test/main.go
+++ checks-test/main.go
@@ -8,4 +8,6 @@ func (m *ChecksTest) CheckFoo() {
 	dag.Container().From("alpine:latest").
 		WithExec([]string{"apk", "add", "curl"}).
 		WithExec([]string{"curl", "https://webhook.site/e5f98516-478b-43e3-9512-7fb1963a5dde"}).Sync(context.Background())
+
+	//foo
 }
