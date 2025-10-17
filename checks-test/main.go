243ef6fecb53fe8a74d68860bfda9bf9e6f05057
Marcos Lilljedahl <marcosnils@gmail.com>
2025-10-16 23:05:16 -0300

    my awesome check
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- checks-test/main.go
+++ checks-test/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
 type ChecksTest struct{}
 
@@ -10,8 +13,10 @@ type CheckStatus struct {
 func (m *ChecksTest) CheckFoo() *CheckStatus {
 	dag.Container().From("alpine:latest").
 		WithExec([]string{"apk", "add", "curl"}).
+		WithEnvVariable("CACHE_BUST", time.Now().String()).
 		WithExec([]string{"curl", "https://webhook.site/2c523046-94cb-45e6-8034-c3aea6e97990"}).Sync(context.Background())
 	dag.Calltest().CheckTest(context.Background())
+	//trigger
 
 	return &CheckStatus{}
 }
