b278c1b133f3c78e453720fae81c0a2f53505155
Marcos Lilljedahl <marcosnils@gmail.com>
2025-10-16 22:24:50 -0300

    test module
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- calltest/main.go
+++ calltest/main.go
@@ -1,17 +1,3 @@
-// A generated module for Calltest functions
-//
-// This module has been generated via dagger init and serves as a reference to
-// basic module structure as you get started with Dagger.
-//
-// Two functions have been pre-created. You can modify, delete, or add to them,
-// as needed. They demonstrate usage of arguments and return types using simple
-// echo and grep commands. The functions can be called from the dagger CLI or
-// from one of the SDKs.
-//
-// The first line in this comment block is a short description line and the
-// rest is a long description with more detail on the module's purpose or usage,
-// if appropriate. All modules should have a short description.
-
 package main
 
 import (
@@ -21,8 +7,6 @@ import (
 
 type Calltest struct{}
 
-// Returns a container that echoes whatever string argument is provided
-func (m *Calltest) Test(ctx context.Context) {
+func (m *Calltest) CheckTest(ctx context.Context) {
 	fmt.Println("I'm within calltest")
-	dag.Sub().SubTest(ctx)
 }
