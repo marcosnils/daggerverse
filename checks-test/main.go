f18a2efbf5d5a58ea23db67694185d5530ad136d
Marcos Lilljedahl <marcosnils@gmail.com>
2025-10-16 21:55:32 -0300

    add checks call event listener
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- checks-test/main.go
+++ checks-test/main.go
@@ -4,8 +4,12 @@ import "context"
 
 type ChecksTest struct{}
 
-func (m *ChecksTest) CheckFoo() {
+type CheckStatus struct {
+}
+
+func (m *ChecksTest) CheckFoo() *CheckStatus {
 	dag.Container().From("alpine:latest").
 		WithExec([]string{"apk", "add", "curl"}).
 		WithExec([]string{"curl", "https://webhook.site/fc8d859e-6144-4a9f-bb62-af889cb18ab8"}).Sync(context.Background())
+	return &CheckStatus{}
 }
