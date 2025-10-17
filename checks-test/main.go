7d66792a67490cc97a8f28c1fe7ed552a102aaa2
Marcos Lilljedahl <marcosnils@gmail.com>
2025-10-16 22:30:26 -0300

    test scaleout
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- checks-test/main.go
+++ checks-test/main.go
@@ -10,6 +10,8 @@ type CheckStatus struct {
 func (m *ChecksTest) CheckFoo() *CheckStatus {
 	dag.Container().From("alpine:latest").
 		WithExec([]string{"apk", "add", "curl"}).
-		WithExec([]string{"curl", "https://webhook.site/fc8d859e-6144-4a9f-bb62-af889cb18ab8"}).Sync(context.Background())
+		WithExec([]string{"curl", "https://webhook.site/2c523046-94cb-45e6-8034-c3aea6e97990"}).Sync(context.Background())
+	dag.Calltest().CheckTest(context.Background())
+
 	return &CheckStatus{}
 }
