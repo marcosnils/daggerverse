c6165019a572cd391767da17bb91ca30d1c234c9
Marcos Lilljedahl <marcosnils@gmail.com>
2025-10-16 23:47:26 -0300

    test2
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- checks-test/main.go
+++ checks-test/main.go
@@ -10,7 +10,7 @@ type ChecksTest struct{}
 type CheckStatus struct {
 }
 
-func (m *ChecksTest) CheckFoo1() *CheckStatus {
+func (m *ChecksTest) CheckMarcos() *CheckStatus {
 	dag.Container().From("alpine:latest").
 		WithExec([]string{"apk", "add", "curl"}).
 		WithEnvVariable("CACHE_BUST", time.Now().String()).
