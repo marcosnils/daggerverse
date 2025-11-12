5c8a98bad56bdc20e3c1f6b6ef5a3956402154c2
Marcos Lilljedahl <marcosnils@gmail.com>
2025-11-12 16:28:56 -0300

    test commit
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- checks-test/main.go
+++ checks-test/main.go
@@ -11,8 +11,6 @@ type CheckStatus struct {
 }
 
 func (m *ChecksTest) CheckMatias() *CheckStatus {
-	//foo
-	//foo
 	dag.Container().From("alpine:latest").
 		WithExec([]string{"apk", "add", "curl"}).
 		WithEnvVariable("CACHE_BUST", time.Now().String()).
