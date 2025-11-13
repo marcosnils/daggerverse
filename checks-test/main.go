b979aff0ebabe798859a215a7fe6fa4412b6866d
Marcos Lilljedahl <marcosnils@gmail.com>
2025-11-13 18:09:00 -0300

    test commit
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- checks-test/main.go
+++ checks-test/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"time"
 )
 
 type ChecksTest struct{}
@@ -10,11 +9,7 @@ type ChecksTest struct{}
 type CheckStatus struct {
 }
 
-func (m *ChecksTest) CheckMatias() *CheckStatus {
-	dag.Container().From("alpine:latest").
-		WithExec([]string{"apk", "add", "curl"}).
-		WithEnvVariable("CACHE_BUST", time.Now().String()).
-		WithExec([]string{"curl", "https://webhook.site/12f90a42-6a19-4864-86bf-f81f841ccc96"}).Sync(context.Background())
-	dag.Calltest().CheckTest(context.Background())
+func (m *ChecksTest) CheckMatias(ctx context.Context) *CheckStatus {
+	dag.Scaleout().Work(ctx)
 	return &CheckStatus{}
 }
